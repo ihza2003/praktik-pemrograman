df166e0fd405449e878cd43b2e116afbbe2c30ba
ihza2003 <ihzahafidzrobbani02@gmail.com>
2021-09-14 21:54:43 +0700

    tugas prkatik pemrograman (pert.3)

---
+++ pertemuan 3/Tugasiden.java
@@ -0,0 +1,25 @@
+public class Tugasiden {
+    public static void main(String[] args) {
+        String salamPembuka = "Assalamualaikum Warahmatullahi Wabarakatuh";
+        String nama = "Ihza Hafidz Robbani";
+        String namaPanggilan = "Ihza";
+        String nomorIndukMahasiswa = "21537144035";
+        String programStudi = "Teknologi Informasi";
+        int tahunAngkatan = 2021;
+        String jurusan = "JPTEI";
+        String fakultas = "Teknik";
+        double ipk = 3.75;
+        String salamPenutup = "Wassalamualaikum Warahmatullahi Wabarakatuh";
+        System.out.println(salamPembuka);
+        System.out.println("perkenalkan nama saya " + nama);
+        System.out.println("Bisa dipanggil " + namaPanggilan);
+        System.out.println("Nim yang saya miliki selama menjadi mahasiswa UNY yaitu " + nomorIndukMahasiswa);
+        System.out.println("saya merupakan mahasiswa prodi " + programStudi);
+        System.out.println("saya dari angkatan " + tahunAngkatan +  "yang merupakan angkatan TI ketiga di UNY ");
+        System.out.println("saya mahasiswa dari jurusan " + jurusan);
+        System.out.println("fakultas " + fakultas);
+        System.out.println("selama saya di UNY selalu berharap mendapatkan ipk yang bagus yaitu " + ipk);
+        System.out.println("sekian perkenalan saya " + salamPenutup);
+    }
+}
+
---
+++ pertemuan 3/Tugasiden.py
@@ -0,0 +1,20 @@
+salamPembuka = "Assalamualaikum Warahmatullahi Wabarakatuh"
+namaLengkap = "Ihza Hafidz Robbani"
+namaPanggilan = "Ihza"
+nomorIndukMahasiswa = 21537144035
+programStudi = "Teknologi Informasi"
+tahunAngkatan = 2021
+jurusan = "JPTEI"
+fakultas = "Teknik"
+ipk = 3.75
+salamPenutup = "Wassalamualaikum warahmatullahi wabarakatuh"
+print(salamPembuka)
+print("perkenalkan nama saya " + namaLengkap)
+print("Bisa dipanggil " + namaPanggilan)
+print("Nim yang saya miliki selama menjadi mahasiswa UNY yaitu " + str(nomorIndukMahasiswa))
+print("saya merupakan mahasiswa prodi " + programStudi)
+print("saya dari angkatan " + str(tahunAngkatan) +  "yang merupakan angkatan TI ketiga di UNY ")
+print("saya mahasiswa dari jurusan " + jurusan)
+print("fakultas " + fakultas)
+print("selama saya di UNY selalu berharap mendapatkan ipk yang bagus yaitu " + str(ipk))
+print("sekian perkenalan saya " + salamPenutup)
