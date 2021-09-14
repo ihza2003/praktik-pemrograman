ee1df4669adc37424ec82ef78f3fff6fea9436cd
ihza2003 <ihzahafidzrobbani02@gmail.com>
2021-09-14 16:02:12 +0700

    menulis identitas

---
+++ pertemuan 3/CobaVar.java
@@ -0,0 +1,10 @@
+public class CobaVar {
+    public static void main(String[] args) {
+        String namaDepan = "Ihza";
+        int umur = 18;
+        double tinggiBadan = 168;
+        System.out.println("Nama Saya " + namaDepan);
+        System.out.println("Umur saya " + umur);
+        System.out.println("tinggi badan " + (int) tinggiBadan);
+    }
+}
