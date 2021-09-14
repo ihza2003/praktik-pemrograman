61692dcaabc9a1bf7f5d3185a13f1076dd73e7ab
ihza2003 <ihzahafidzrobbani02@gmail.com>
2021-09-14 16:33:27 +0700

    identitas python

---
+++ pertemuan 3/CobaVar.py
@@ -0,0 +1,9 @@
+namaDepan = "Ihza"
+umur = 18
+tinggiBadan = 168
+print("Nama saya adalah " + namaDepan)
+print("Umur saya adalah " + str(umur) + " tahun ")
+print(tinggiBadan)
+print(type(namaDepan))
+print(type(umur))
+print(type(tinggiBadan))
